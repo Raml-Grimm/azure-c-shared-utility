a6869182bbaae18b2bc1747a2d4cc2ee5a19b04f
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2018-11-02 15:48:16 -0700

    Fix candidate for MBED size_t build failure

--- inc/azure_c_shared_utility/strings.h
+++ inc/azure_c_shared_utility/strings.h
@@ -4,16 +4,16 @@
 #ifndef STRINGS_H
 #define STRINGS_H
 
-#include "azure_c_shared_utility/umock_c_prod.h"
-#include "azure_c_shared_utility/strings_types.h"
 
 #ifdef __cplusplus
 #include <cstddef>
 extern "C"
 {
-#else
-#include <stddef.h>
 #endif
+
+#include <stddef.h>
+#include "azure_c_shared_utility/umock_c_prod.h"
+#include "azure_c_shared_utility/strings_types.h"
 
 MOCKABLE_FUNCTION(, STRING_HANDLE, STRING_new);
 MOCKABLE_FUNCTION(, STRING_HANDLE, STRING_clone, STRING_HANDLE, handle);
